563a746c9d4d3bb1386e9172a7f2e755ad17dd08
Jarzykk <63424064+Jarzykk@users.noreply.github.com>
2021-01-06 14:09:18 +0200

    First commit
    Added project to Git.

---
+++ Alarm/Assets/Scripts/Alarm.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class Alarm : MonoBehaviour
+{
+    private bool _alarmActive = false;
+    private bool _decreaseAlarmVolume = true;
+    private AudioSource _alarmSound;
+    private float _alarmMuteTime = 1.5f;
+    private float _alarmMuteTimePassed;
+
+    void Start()
+    {
+        _alarmSound = GetComponent<AudioSource>();
+    }
+
+    void Update()
+    {
+        if(_alarmActive)
+        {
+            if(_decreaseAlarmVolume == true)
+            {
+                ChangeSoundVolume(_alarmSound, 1, 0.0000f);
+            }
+            else if(_decreaseAlarmVolume == false)
+            {
+                ChangeSoundVolume(_alarmSound, 0, 1f);
+            }
+
+            if (_alarmSound.volume == 0)
+                _decreaseAlarmVolume = false;
+            else if (_alarmSound.volume == 1)
+                _decreaseAlarmVolume = true;
+        }
+
+        Debug.Log(_decreaseAlarmVolume);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.TryGetComponent<Player>(out Player player))
+            _alarmActive = !_alarmActive;
+
+        if (_alarmActive)
+            _alarmSound.Play();
+        else
+            _alarmSound.Stop();
+    }
+
+    private void ChangeSoundVolume(AudioSource sound, float volumeAtBeggining, float volumeAtEnd)
+    {
+        _alarmMuteTimePassed += Time.deltaTime;
+        sound.volume = Mathf.Lerp(volumeAtBeggining, volumeAtEnd, _alarmMuteTimePassed / _alarmMuteTime);
+
+        if (_alarmMuteTimePassed >= _alarmMuteTime)
+            _alarmMuteTimePassed = 0;
+    }
+}
---
+++ Alarm/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float _movementSpeed;
+
+    private Rigidbody2D _rb;
+    private Vector2 _moveVelocity;
+
+    private void Start()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        Vector2 moveInput = new Vector2(Input.GetAxis("Horizontal"), 0);
+        _moveVelocity = moveInput * _movementSpeed;
+    }
+
+    private void FixedUpdate()
+    {
+        _rb.position += _moveVelocity * Time.deltaTime;
+    }
+}
